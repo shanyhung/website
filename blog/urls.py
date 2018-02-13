2c1eff05c4a6f75591548188d7b8c52276d44d06
shanyhung <shany.hunghc@gmail.com>
2018-02-14 01:10:03 +0800

    add linearRegression.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list),
+    url(r'^linearRegression$', views.linearRegression, name='linearRegression'),
 ]
